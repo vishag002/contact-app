036035601ea5b697f88869a46f9db05c6861fdca
vishag002 <vvishag477@gmail.com>
2024-06-13 22:09:05 +0530

    text_constant

---
+++ lib/utilits/text_const/text_constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextConstant {
+  static TextStyle style1 = GoogleFonts.sourceCodePro(
+    color: Colors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+}
