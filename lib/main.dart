44b9e105dd76dd4826769f41591d3e4c2f78b3f1
vishag002 <vvishag477@gmail.com>
2024-06-14 22:53:20 +0530

    added provider and hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+// lib/main.dart
+import 'package:contact_application/controller/contact_provider.dart';
+import 'package:contact_application/controller/student_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+import 'model/contact.dart';
 import 'view/home_screen.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(ContactAdapter());
+  boxContact = await Hive.openBox<Contact>('contactBox');
+  runApp(ChangeNotifierProvider(
+    create: (context) => ContactProvider(),
+    child: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
