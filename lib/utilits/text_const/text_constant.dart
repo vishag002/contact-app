6bfa981981628554f4ee4b7a4cb133137e8a8f9d
vishag002 <vvishag477@gmail.com>
2024-06-15 19:31:20 +0530

    text_constant

--- lib/utilits/text_const/text_constant.dart
+++ lib/utilits/text_const/text_constant.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class TextConstant {
   static TextStyle style1 = GoogleFonts.sourceCodePro(
     color: Colors.black,
     fontSize: 18,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleHeading = GoogleFonts.signika(
+    color: Colors.grey.shade100,
+    fontSize: 25,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleContactName = GoogleFonts.signika(
+    color: Colors.black,
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle style91 = GoogleFonts.gelasio(
+    color: Colors.black,
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle styleNumber = GoogleFonts.alice(
+    color: Colors.black,
+    fontSize: 22,
     fontWeight: FontWeight.w500,
   );
 }
