bc7dbf4d692e836f24dbad4ddb2a31e8261f4924
vishag002 <vvishag477@gmail.com>
2024-06-15 19:31:07 +0530

    color_constant

---
+++ lib/utilits/color_const/color_constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ColorConstant {
+  static const Color background = Colors.black;
+  static const Color primary = Color.fromARGB(255, 235, 232, 232);
+}
