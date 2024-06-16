3198f7c67e314f5f909280dbbd39c593212bc512
vishag002 <vvishag477@gmail.com>
2024-06-16 19:31:08 +0530

    added validation

--- lib/view/add_contact_screen.dart
+++ lib/view/add_contact_screen.dart
@@ -2,6 +2,7 @@ import 'package:contact_application/controller/contact_provider.dart';
 import 'package:contact_application/utilits/color_const/color_constant.dart';
 import 'package:contact_application/utilits/text_const/text_constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class AddContactScreen extends StatefulWidget {
@@ -13,6 +14,7 @@ class AddContactScreen extends StatefulWidget {
 
 class _AddContactScreenState extends State<AddContactScreen> {
   //
+  final validationKey = GlobalKey<FormState>();
   final nameController = TextEditingController();
   final surnameController = TextEditingController();
   final phoneController = TextEditingController();
@@ -59,11 +61,12 @@ class _AddContactScreenState extends State<AddContactScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: IconButton(
               onPressed: () {
-                Provider.of<ContactProvider>(context, listen: false).addContact(
-                    nameController.text,
-                    surnameController.text,
-                    phoneController.text);
-                Navigator.pop(context);
+                if (validationKey.currentState!.validate()) {
+                  Provider.of<ContactProvider>(context, listen: false)
+                      .addContact(nameController.text, surnameController.text,
+                          phoneController.text);
+                  Navigator.pop(context);
+                }
               },
               icon: Icon(
                 Icons.check_sharp,
@@ -76,54 +79,83 @@ class _AddContactScreenState extends State<AddContactScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "  Name",
-              style: TextConstant.style1,
-            ),
-            TextFormField(
-              controller: nameController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  gapPadding: 20,
-                ),
-                hintText: 'Enter name',
+        child: Form(
+          key: validationKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "  Name",
+                style: TextConstant.style1,
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "  Surname",
-              style: TextConstant.style1,
-            ),
-            TextFormField(
-              controller: surnameController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  gapPadding: 20,
+              TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    gapPadding: 20,
+                  ),
+                  hintText: 'Enter name',
                 ),
-                hintText: 'Enter Surname',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "enter name to save contact";
+                  } else {
+                    return null;
+                  }
+                },
               ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "  Phone Number",
-              style: TextConstant.style1,
-            ),
-            TextFormField(
-              controller: phoneController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                  gapPadding: 20,
+              SizedBox(height: 20),
+              Text(
+                "  Surname",
+                style: TextConstant.style1,
+              ),
+              TextFormField(
+                controller: surnameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    gapPadding: 20,
+                  ),
+                  hintText: 'Enter Surname',
                 ),
-                hintText: '+91 _ _ _ _ _ _ _ _ _',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "enter a valid name";
+                  } else {
+                    return null;
+                  }
+                },
               ),
-            )
-          ],
+              SizedBox(height: 20),
+              Text(
+                "  Phone Number",
+                style: TextConstant.style1,
+              ),
+              TextFormField(
+                maxLength: 10,
+                maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                controller: phoneController,
+                decoration: InputDecoration(
+                  counterText: "",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    gapPadding: 20,
+                  ),
+                  hintText: '+91 _ _ _ _ _ _ _ _ _',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "phone number is required";
+                  } else if (!RegExp(r'^\d{10}$').hasMatch(value)) {
+                    return 'Enter a valid 10-digit phone number';
+                  }
+                  return null;
+                },
+                keyboardType: TextInputType.phone,
+              )
+            ],
+          ),
         ),
       ),
     );
